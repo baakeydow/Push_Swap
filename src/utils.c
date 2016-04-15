79ac82a25e7e205b8395e943b2b08efcb6e9cdc8
Bilal NDAO <bndao@student.42.fr>
2016-04-15 07:05:49 +0200

    cleaned up

--- src/utils.c
+++ src/utils.c
@@ -30,3 +30,45 @@ int								size_list(t_lst *l)
 {
 	return (l) ? (size_list(l->next) + 1) : (0);
 }
+
+t_lst							*last_is_first(t_lst *start)
+{
+	t_lst		*nextone;
+	t_lst		*begin;
+
+	begin = start;
+	nextone = start->next;
+	while (start && start->next != NULL)
+	{
+		swap_elem_data(start, nextone);
+		start = start->next;
+		nextone = nextone->next;
+	}
+	nextone = begin;
+	start = begin;
+	return (start);
+}
+
+t_lst							*swap_loop(t_lst *start)
+{
+	t_lst		*prevone;
+	t_lst		*begin;
+	t_lst		*end;
+	t_lst		*tmp;
+
+	begin = start;
+	tmp = start;
+	while (tmp->next)
+		tmp = tmp->next;
+	end = tmp;
+	prevone = end->prev;
+	while (end && end->prev != NULL)
+	{
+		swap_elem_data(end, prevone);
+		end = end->prev;
+		prevone = prevone->prev;
+	}
+	prevone = begin;
+	end = begin;
+	return (end);
+}
